4add146a6ead9f2581b46412af71b75bcbe7c1b7
qifen <858810078@qq.com>
2022-01-27 21:13:23 +0800

    signal

--- widget.py
+++ widget.py
@@ -3,9 +3,11 @@ import os
 from pathlib import Path
 import sys
 
-from PySide6.QtWidgets import QApplication, QWidget
+
+from PySide6.QtWidgets import QApplication, QWidget, QMessageBox
 from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
+from PyQt5.QtCore import QObject, pyqtSignal
 
 
 class Widget(QWidget):
@@ -13,17 +15,40 @@ class Widget(QWidget):
         super(Widget, self).__init__()
         self.load_ui()
 
+
+
+
     def load_ui(self):
         loader = QUiLoader()
         path = os.fspath(Path(__file__).resolve().parent / "form.ui")
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
+        root = loader.load(ui_file, self)
         ui_file.close()
+#发送消息
+class Qbj1(QObject):
+    sign1 = pyqtSignal(float);
+    def __init__(self):
+        super(Qbj1,self).__init__()
+    def sendSign(self):
+        print("send message====")
+        self.sign1.emit(999)
+#接受消息
+class Qbj2(QObject):
+    def __init__(self):
+        super(Qbj2,self).__init__()
+    def receiverSign(self,value):
+        print("receiver signal=======")
+        print(value)
 
 
 if __name__ == "__main__":
     app = QApplication([])
+
+    obj1 = Qbj1()
+    obj2 = Qbj2()
+    obj1.sign1.connect(obj2.receiverSign)
+    obj1.sendSign()
     widget = Widget()
     widget.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
